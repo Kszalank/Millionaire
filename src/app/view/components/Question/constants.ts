341614f24add26b78a4c4cf1c61b379cc1438651
Kszalank <szalankiewiczkrzysiek@gmail.com>
2022-11-05 22:05:32 +0100

    Questions List added

---
+++ src/app/view/components/Question/constants.ts
@@ -0,0 +1,103 @@
+const QuestionsList = [
+  {
+    question: " From 1964 to 1985, who asked you not to squeeze the Charmin?",
+    wrongAnswers: ["Mrs. Potts", "Mr. Rogers", "Mrs. Fields"],
+    rightAnswer: "Mr. Whipple",
+  },
+  {
+    question: "Nadia Comaneci was the first gymnast to ever do what at the Olymphics?",
+    wrongAnswers: ["Finish with a broken ankle", "Win an Olympic gold medal", "Forfit her position"],
+    rightAnswer: "Get a perfect 10",
+  },
+  {
+    question: "Which of these birds has the biggest brain relative to its size?",
+    wrongAnswers: ["Ostrich", "Sparrow", "Eagle"],
+    rightAnswer: "Hummingbird",
+  },
+  {
+    question: "When playing Blackjack, how many points would be considered a bust",
+    wrongAnswers: ["21", "15", "19"],
+    rightAnswer: "22",
+  },
+  {
+    question: "Oysters can change something most other animals cannot. What is it?",
+    wrongAnswers: ["Their size", "The thickness of their shell", "Their color"],
+    rightAnswer: "Their gender",
+  },
+  {
+    question: "The Statue of Liberty was originally supposed to function as what?",
+    wrongAnswers: ["A port of entry", "A border marker", "A gift shop"],
+    rightAnswer: "A lighthouse",
+  },
+  {
+    question: "Which of these U.S. state names has the fewest syllables?",
+    wrongAnswers: ["Iowa", "Ohio", "Texas"],
+    rightAnswer: "Maine",
+  },
+  {
+    question: `"Heart of Glass" was a single recorded by which 70s band?`,
+    wrongAnswers: ["The Ramones", "Joy Division", "The Bee Gees"],
+    rightAnswer: "Blondie",
+  },
+  {
+    question: "Which one of these historical meanies died from a nosebleed?",
+    wrongAnswers: ["Napoleon", "Adolf Hitler", "Marie Antionette"],
+    rightAnswer: "Atilla the Hun",
+  },
+  {
+    question: `Which of these rock groups released an album titled "The Joshua Tree"?`,
+    wrongAnswers: ["Kings of Leon", "Led Zeppelin", "Arcade Fire"],
+    rightAnswer: "U2",
+  },
+  {
+    question: `What was Will Ferrell's character's name in the 2003 hit movie "Elf"?`,
+    wrongAnswers: ["Elf", "Sam", "Billy"],
+    rightAnswer: "Buddy",
+  },
+  {
+    question: "Which of these lazy animals sleeps up to 22 hours a day?",
+    wrongAnswers: ["Lion", "Copperhead", "Beagle"],
+    rightAnswer: "Koala",
+  },
+  {
+    question: "Where did Noah's Ark come to rest after the flood?",
+    wrongAnswers: ["Mount Everest", "Mount Fuji", "K2"],
+    rightAnswer: "Mount Ararat",
+  },
+  {
+    question: "How many years are there in an eon?",
+    wrongAnswers: ["35 years", "1 000 years", "25 000 years"],
+    rightAnswer: "1 billion years",
+  },
+  {
+    question: "Out of these European countries, which has the smallest population density?",
+    wrongAnswers: ["Bulgaria", "Norway", "France"],
+    rightAnswer: "Iceland",
+  },
+
+  {
+    question: `Do you know which U.S. newspaper is nicknamed "The Gray Lady"?`,
+    wrongAnswers: ["The Daily Mail", "The Washington Post", "USA Today"],
+    rightAnswer: "The New York Times",
+  },
+  {
+    question: `According to Albert Einstein, what is the "hardest thing in the world to understand"?`,
+    wrongAnswers: ["Astronomy", "Physics", "Sewing"],
+    rightAnswer: "Income taxes",
+  },
+  {
+    question: "What was the singer Punk named at birth?",
+    wrongAnswers: ["Madonna Ciccone", "Stefani Joanne Angelina Germanotta", "Robyn Fenty"],
+    rightAnswer: "Alecia Moore",
+  },
+  {
+    question: "When you cyt into an onion which element causes it to smell?",
+    wrongAnswers: ["Plutonium", "Potassium", "Hydrogen"],
+    rightAnswer: "Sulfur",
+  },
+  {
+    question: "Which part of your body has the more sweat glands than the others?",
+    wrongAnswers: ["Head", "Armpits", "Back"],
+    rightAnswer: "Feet",
+  },
+];
